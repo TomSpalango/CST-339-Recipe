09c2dfa4e01a28db344d0680994f0bab9c4dff03
Ty <thetyg@outlook.com>
2025-01-19 14:19:34 -0700

    Main Application Module

---
+++ MainApp/src/main/java/com/example/model/MainController.java
@@ -0,0 +1,15 @@
+package com.example.model;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage(Model model) {
+        model.addAttribute("title", "Welcome to SavoryScript");
+        return "index";
+    }
+}
