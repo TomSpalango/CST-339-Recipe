32ffe863ffc1505b013ff8e9bc8d24689185fac1
Selinebowens <nytokumu@gmail.com>
2025-01-19 21:09:08 -0500

    Add files via upload

---
+++ SavoryScripts/src/main/java/com/gcu/savoryscripts/model/User.java
@@ -0,0 +1,26 @@
+package com.gcu.savoryscripts.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+public class User {
+	
+	
+	@NotEmpty(message = "First name is required")
+	private String firstName;
+	
+	@NotEmpty(message = "Last name is required")
+    private String lastName;
+	
+	@Email(message = "Valid Email is required")
+    private String email;
+	
+	@NotEmpty(message = "Phone number is required")
+    private String phone;
+	
+	@NotEmpty(message = "Username is required")
+    private String username;
+	 
+	 @NotEmpty(message = "Password is required")
+    private String password;
+}
