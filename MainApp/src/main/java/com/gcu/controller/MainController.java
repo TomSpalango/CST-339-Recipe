dd777b8620734d322b9753e5a18ca801c0545fae
Tom Spalango <tom@spalango.com>
2025-01-20 00:23:05 -0500

    Updated MainController to handle /, /login, /register

--- MainApp/src/main/java/com/gcu/controller/MainController.java
+++ MainApp/src/main/java/com/gcu/controller/MainController.java
@@ -6,9 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
     @GetMapping("/")
     public String mainPage(Model model) {
         model.addAttribute("title", "Welcome to SavoryScript");
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register";
     }
 }
