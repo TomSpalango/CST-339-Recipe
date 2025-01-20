0c6f98524d226e7a4df0e46e204874b7eec3a7c3
Tom Spalango <tom@spalango.com>
2025-01-20 00:18:59 -0500

    MainController will be the main entry point - HomeController is redundant

--- MainApp/src/main/java/com/gcu/controller/MainController.java
+++ MainApp/src/main/java/com/gcu/controller/MainController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-
     @GetMapping("/")
     public String mainPage(Model model) {
         model.addAttribute("title", "Welcome to SavoryScript");
