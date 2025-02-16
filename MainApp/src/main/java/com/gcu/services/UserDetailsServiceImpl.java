f78b13b87c4d6011892c3c5464569769fbe406c0
Seline Bowens <nytokumu@gmail.com>
2025-02-16 02:47:20 -0500

    updated

---
+++ MainApp/src/main/java/com/gcu/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.gcu.services;
+
+import com.gcu.model.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.gcu.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        UserBuilder builder = org.springframework.security.core.userdetails.User.withUsername(username);
+        builder.password(user.getPassword());
+        builder.roles("USER");
+        return builder.build();
+    }
+
+}
